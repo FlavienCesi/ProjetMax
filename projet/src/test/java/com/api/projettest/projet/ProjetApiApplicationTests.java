17f65c29f8ef2aba17a10a0bcdd2917c54a29026
FlavienCesi <flavien.mazier@viacesi.fr>
2023-03-11 22:29:11 +0100

    ajout de tout

---
+++ projet/src/test/java/com/api/projettest/projet/ProjetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.projettest.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
